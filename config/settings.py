2c8ad4e4d0debfc30262a3999acd5238fbc71b22
universalsigmateknologi <universalsigmateknologi@gmail.com>
2026-07-14 14:05:45 +0700

    komunikasi apps

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'accounts',
     'master',
     'jurnal',
+    'prakerin',
 ]
 
 MIDDLEWARE = [
---
+++ komunikasi/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class KomunikasiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'komunikasi'
